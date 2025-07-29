8956c05c38ae69e51cb381aacf953a70c6d75bc1
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-07-28 21:01:22 -0400

    Day 02

--- 01_Java_Basics/02_The Basics of Java/Day02_Starter_Code.java
+++
@@ -1,9 +0,0 @@
-public class AboutMe {
-    public static void main(String[] args) {
-        // TODO: Declare variables for your name, age, and a fun fact
-        // Example:
-        // String name = "Your Name";
-        // int age = 16;
-        // String funFact = "I can juggle!";
-    }
-}
