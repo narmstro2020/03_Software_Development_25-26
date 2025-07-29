67c8d019c83617669aed6ab0ab4059b9ed7c640a
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-07-29 13:49:42 -0400

    updates for Day 02

---
+++ 01_Java_Basics/02_The Basics of Java/S1D2LectureCode.java
@@ -0,0 +1,78 @@
+public class S1D2LectureCode {
+
+    public static void main(String[] args) {
+        byte myByte = 32;
+        System.out.print("My byte is: ");
+        System.out.println(myByte);
+        System.out.print("The smallest byte is: ");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.print("The largest byte is: ");
+        System.out.println(Byte.MAX_VALUE);
+
+        short myShort = 3270;
+        System.out.print("My short is: ");
+        System.out.println(myShort);
+        System.out.print("The smallest short is: ");
+        System.out.println(Short.MIN_VALUE);
+        System.out.print("The largest short is: ");
+        System.out.println(Short.MAX_VALUE);
+
+        int myInt = 3270;
+        System.out.print("My int is: ");
+        System.out.println(myInt);
+        System.out.print("The smallest int is: ");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.print("The largest int is: ");
+        System.out.println(Integer.MAX_VALUE);
+
+        long myLong = 2304987230498237098L;
+        System.out.print("My long is: ");
+        System.out.println(myLong);
+        System.out.print("The smallest long is: ");
+        System.out.println(Long.MIN_VALUE);
+        System.out.print("The largest long is: ");
+        System.out.println(Long.MAX_VALUE);
+
+        float myFloat = 3.14926535f;
+        System.out.print("My float is: ");
+        System.out.println(myFloat);
+        System.out.print("The smallest float is: ");
+        System.out.println(Float.MIN_VALUE);
+        System.out.print("The largest float is: ");
+        System.out.println(Float.MAX_VALUE);
+
+        double myDouble = 3.14159265358979323;
+        System.out.print("My double is: ");
+        System.out.println(myLong);
+        System.out.print("The smallest double is: ");
+        System.out.println(Double.MIN_VALUE);
+        System.out.print("The largest double is: ");
+        System.out.println(Double.MAX_VALUE);
+
+        double myDouble2 = 1.0 / 2.0;
+        System.out.print("My double 2 is: ");
+        System.out.println(myDouble2);
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+        System.out.print("The smallest double is: ");
+
+        char myChar = '\u03B8';
+        System.out.print("My char is: ");
+        System.out.println(myChar);
+        System.out.print("The smallest char is: ");
+        System.out.println(Character.MIN_VALUE);
+        System.out.print("The largest char is: ");
+        System.out.println(Character.MAX_VALUE);
+
+        String myString = "Hello Reference";
+        System.out.print("My string is: ");
+        System.out.println(myString);
+
+        myString = "Hi Java";
+        System.out.print("My string is: ");
+        System.out.println(myString);
+
+    }
+
+}
